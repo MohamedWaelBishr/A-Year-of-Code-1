9592a4a26217f845f44081400af11000185fcfa7
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-03-11 13:13:10 +0200

    Day 8

--- e_sin_x.m
+++ e_sin_x.m
@@ -8,7 +8,7 @@ plot(x,y), xlabel('x'),ylabel('exp(–1.5x)*sin(10x)'),axis([0 5 -1 1])
 
 
 %Plot 2
-y = exp(-2*x);
+y = exp(-2*x); 
 
 subplot(2,2,3)
 plot(x,y),xlabel('x'),ylabel('exp(–2x)'),axis([0 5 -1 1])
