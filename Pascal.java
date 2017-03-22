ba9a40231d1d06908ebadb9c5010bced2dd99dfe
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-03-22 19:02:51 +0200

    Day 18

--- Pascal.java
+++ Pascal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class JavaPascalTriangle {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) { 
 
         int r, i, k, number = 1, j;
         Scanner scan = new Scanner(System.in);
