2682f07b6b1f41d103216c181686cc6e59c6abe1
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-03-23 08:57:39 +0200

    Day 19

---
+++ matrix.m
@@ -0,0 +1,19 @@
+clear;
+
+a = [ 1 2 3 ; 4 5 6; 7 8 9];
+b = [ 7 5 6 ; 2 0 8; 5 7 1];
+
+%Addition 
+c = a + b
+
+%Subtraction
+d = a - b
+
+%Division
+e = a / b
+
+%Transpose
+f = a'
+
+%Determinant
+g = det(a)
