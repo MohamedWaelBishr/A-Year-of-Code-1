8d08983ae4baee2ca08cc7c3c028d1a0a5b2b2ca
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-03-26 09:29:56 +0200

    Day 22

---
+++ Singleton.java
@@ -0,0 +1,20 @@
+package com.rashad.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+    
+    private Singleton(){}
+    
+    static{
+        try{
+            instance = new Singleton();
+        }catch(Exception e){
+            throw new RuntimeException("Exception occured in creating singleton instance");
+        }
+    }
+    
+    public static Singleton getInstance(){
+        return instance;
+    }
+}
