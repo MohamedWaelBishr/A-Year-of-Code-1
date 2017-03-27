ef09414cc1d7d88139d958ecd058c9cdf9283744
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-03-27 09:30:20 +0200

    Day 23

---
+++ half_adder.vhd
@@ -0,0 +1,10 @@
+entity HALF_ADDER is
+  port(A,   B:     in  bit;
+       SUM, CARRY: out bit);
+end HALF_ADDER;
+
+architecture DATAFLOW of HALF_ADDER is
+begin
+    SUM   <= A xor B;
+    CARRY <= A and B;
+end DATAFLOW;
