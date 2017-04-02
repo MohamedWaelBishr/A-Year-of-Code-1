e785e8daf6e047ff335e9609299ea7b171a2e60d
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-04-02 19:34:37 +0200

    Day 29

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
+
+alist = [542346,924323,12343247,7347,3341,4344,543245,234230]
+selectionSort(alist)
+print(alist)
