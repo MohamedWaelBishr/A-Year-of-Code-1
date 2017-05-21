d8a7801d272e0d136d9c282eba14321f8826e1a8
Mohamed Rashad <thegreatrashad@yahoo.com>
2017-05-21 17:13:43 +0200

    Day 78

---
+++ EnumExample.java
@@ -0,0 +1,40 @@
+public enum Directions{
+	  EAST, 
+	  WEST, 
+	  NORTH, 
+	  SOUTH
+}
+
+public class EnumExample
+{
+   Directions dir;
+   public EnumExample(Directions dir) {
+      this.dir = dir;
+   }
+   public void getMyDirection() {
+     switch (dir) {
+       case EAST:
+          System.out.println("In East Direction");
+          break;
+                    
+       case WEST:
+          System.out.println("In West Direction");
+          break;
+                         
+       case NORTH: 
+          System.out.println("In North Direction");
+          break;
+                        
+       default:
+          System.out.println("In South Direction");
+          break;
+     }
+   }
+    
+    public static void main(String[] args) {
+        EnumExample obj1 = new EnumDemo(Directions.EAST);
+        obj1.getMyDirection();
+        EnumExample obj2 = new EnumDemo(Directions.SOUTH);
+        obj2.getMyDirection();
+    }
+}
